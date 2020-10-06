13a9dbbf5ab17cb7af33b9c227da7229558845e5
wuxuan <937626344@qq,com>
2020-10-06 17:58:15 +0800

    Default Changelist

---
+++ Codes/llcreate.c
@@ -0,0 +1,96 @@
+//
+// Created by 武轩 on 6/10/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct Node *LinkedNode;
+
+typedef struct Node {
+    int value;
+    LinkedNode next;
+};
+
+typedef struct List *LinkedList;
+
+struct List {
+    LinkedNode first;
+    LinkedNode last;
+    int allnumber;
+};
+
+LinkedNode CreatedNode(int v){
+    LinkedNode new = malloc(sizeof(struct Node));
+    new->value = v;
+    new->next = NULL;
+    return new;
+}
+
+LinkedList CreatedList(){
+    LinkedList new = malloc(sizeof(struct List));
+    new->first = NULL;
+    new->last = NULL;
+    new->allnumber = 0;
+    return new;
+}
+
+void joinLL(LinkedList LL, int data) {
+    assert(LL != NULL);
+    LinkedNode new = CreatedNode(data);
+    if (LL->first == NULL) {
+        LL->first = new;
+        LL->last = new;
+        LL->allnumber++;
+    } else {
+        LL->last->next = new;
+        LL->last = new;
+        LL->allnumber++;
+    }
+}
+
+void freeLL(LinkedList ll) {
+    assert(ll != NULL);
+    LinkedNode temp = ll->first;
+    while(temp!=NULL) {
+        LinkedNode next = temp->next;
+        free(temp);
+        temp = next;
+    }
+    free(ll);
+}
+
+
+void showLL(LinkedList ll) {
+    assert(ll != NULL);
+    LinkedNode p = ll->first;
+    printf("Done. ");
+    if (p != NULL) {
+        printf("List is ");
+    }
+    for(p;p->next!=NULL;p=p->next){
+        printf("%d-->",*p);
+    }
+    printf("%d",*p);
+
+}
+int  main(){
+    LinkedList LL=CreatedList();
+    int a;
+    int b;
+    while (1){
+        printf("Enter an integer: ");
+        b=scanf("%d",&a);
+        if(b==1){
+            joinLL( LL,a);
+        }else{
+            break;
+        }
+    }
+    showLL(LL);
+    freeLL(LL);
+    return 0;
+
+}
+
