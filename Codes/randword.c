b1a1c2744137fb58343c6b63a542ce48d59e8998
wuxuan <937626344@qq,com>
2020-11-23 07:07:58 +0800

    Default Changelist

--- Codes/randword.c
+++ Codes/randword.c
@@ -1,14 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 int word(int length, int seed){
     char words[27]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0'};
     char w[length];
+    srand(time(NULL));
     for (int i = 0; i < length; ++i) {
-        w[i]=words[rand()%25];
+        w[i]=words[rand()%26];
     }
     printf("%s",w);
     return 0;
