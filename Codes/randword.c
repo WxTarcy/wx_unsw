3d38a08837bfbad1bee9ec8dadae31a22125eca6
wuxuan <937626344@qq,com>
2020-11-23 06:51:49 +0800

    Default Changelist

--- Codes/randword.c
+++ Codes/randword.c
@@ -6,14 +6,10 @@
 int word(int length, int seed){
     char words[27]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0'};
-    int aa=0;
-    char c;
     char w[length];
     for (int i = 0; i < length; ++i) {
         srand(seed+i);
-        aa=rand()%25;
-        c=words[aa];
-        w[i]=c;
+        w[i]=words[rand()%25];
     }
     printf("%s",w);
     return 0;
