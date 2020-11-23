147cc9210d23859ff653964364b30e1415b947c7
wuxuan <937626344@qq,com>
2020-11-23 13:55:59 +0800

    Default Changelist

--- Codes/randword.c
+++ Codes/randword.c
@@ -24,11 +24,9 @@ int main(int argc, char *argv[]) {
     int seed = atoi(argv[2]);
     char words[27]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0'};
-    char w[length];
     srand(seed);
     for (int i = 0; i < length; i++){
-        w[i]=words[rand()%26];
+        printf("%c",words[rand()%26]);
     }
-    printf("%s",w);
     return 0;
 }
