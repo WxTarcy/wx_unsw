2f3b1c1ca670d4dc624c0ee5a8ff44556a5f7c7a
wuxuan <937626344@qq,com>
2020-11-23 13:54:17 +0800

    Default Changelist

--- Codes/randword.c
+++ Codes/randword.c
@@ -1,14 +1,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
+void word(int length, int seed){
+    char words[27]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0'};
+    char w[length];
+    srand(seed);
+    for (int i = 0; i < length; ++i) {
+        w[i]=words[rand()%26];
+    }
+    printf("%s",w);
+}
+
+
+
 #define BEGIN  97  // ASCII of 'a'
 
 int main(int argc, char *argv[]) {
     int length = atoi(argv[1]);
     int seed = atoi(argv[2]);
+    char words[27]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0'};
+    char w[length];
     srand(seed);
     for (int i = 0; i < length; i++){
-        printf("%c", (rand() % 26 + BEGIN));
+        w[i]=words[rand()%26];
     }
+    printf("%s",w);
     return 0;
 }
